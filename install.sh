84091cf889dd05d1a8ff7cd17facbc39bd819728
Kevin Brack <brackkevin@gmail.com>
2020-08-08 11:29:08 -0500

    Add install script

---
+++ install.sh
@@ -0,0 +1,11 @@
+log_file=~/install_progress_log.txt
+
+sudo apt-get -y install zsh
+if type -p zsh > /dev/null; then
+    echo "zsh Installed" >> $log_file
+else
+    echo "zsh FAILED TO INSTALL!!!" >> $log_file
+fi
+
+sudo apt-get install zsh-syntax-highlighting
+
